6972c98262d45f71737f093c46a8a290239074c1
valer <valerianicoll.calderonr@gmail.com>
2023-01-03 15:23:23 -0500

    add build directory

--- angular14/src/app/app.component.ts
+++ angular14/src/app/app.component.ts
@@ -27,12 +27,12 @@ export class AppComponent {
     },
     initialView: 'dayGridMonth',
     initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
-    weekends: true,
+    weekends: false,
     editable: true,
+    locale: 'es',
     selectable: true,
     selectMirror: true,
     dayMaxEvents: true,
-    select: this.handleDateSelect.bind(this),
     eventClick: this.handleEventClick.bind(this),
     eventsSet: this.handleEvents.bind(this)
     /* you can update a remote database when these fire:
@@ -42,8 +42,9 @@ export class AppComponent {
     */
   };
   currentEvents: EventApi[] = [];
-
+  
   constructor(private changeDetector: ChangeDetectorRef) {
+    
   }
 
   handleCalendarToggle() {
@@ -55,26 +56,11 @@ export class AppComponent {
     calendarOptions.weekends = !calendarOptions.weekends;
   }
 
-  handleDateSelect(selectInfo: DateSelectArg) {
-    const title = prompt('Please enter a new title for your event');
-    const calendarApi = selectInfo.view.calendar;
-
-    calendarApi.unselect(); // clear date selection
-
-    if (title) {
-      calendarApi.addEvent({
-        id: createEventId(),
-        title,
-        start: selectInfo.startStr,
-        end: selectInfo.endStr,
-        allDay: selectInfo.allDay
-      });
-    }
-  }
+  
 
   handleEventClick(clickInfo: EventClickArg) {
-    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-      clickInfo.event.remove();
+    if (confirm(`NUEVO EVENTO DETALLES '${clickInfo.event.title}'`)) {
+      console.log(clickInfo.event.start);
     }
   }
 
--- angular14/src/app/event-utils.ts
+++ angular14/src/app/event-utils.ts
@@ -2,24 +2,29 @@ import { EventInput } from '@fullcalendar/core';
 
 let eventGuid = 0;
 const TODAY_STR = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of today
-
+const DAY_TODAY = new Date().getDay();
 export const INITIAL_EVENTS: EventInput[] = [
   {
     id: createEventId(),
-    title: 'All-day event',
-    start: TODAY_STR
+    title: 'CURSO MACHINE LEARNING',
+    start: TODAY_STR,
+    end: TODAY_STR,
+    number: DAY_TODAY
+    
   },
   {
     id: createEventId(),
-    title: 'Timed event',
+    title: 'CURSO PYTHON PARA EL ANALISIS',
     start: TODAY_STR + 'T00:00:00',
-    end: TODAY_STR + 'T03:00:00'
+    end: TODAY_STR + 'T03:00:00',
+    number: DAY_TODAY
   },
   {
     id: createEventId(),
-    title: 'Timed event',
+    title: 'CURSO BIG DATA',
     start: TODAY_STR + 'T12:00:00',
-    end: TODAY_STR + 'T15:00:00'
+    end: TODAY_STR + 'T15:00:00',
+    number: DAY_TODAY
   }
 ];
 
